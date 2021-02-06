026240583a200c1ac9b9201daabbe14a0d5bff2c
itaweb <admod32@gmail.com>
2021-02-06 23:43:01 +0300

    Create urls to homepage

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html', {'password': 'rty67rjhvb87'})
+
+
+def hello_fun(request):
+    return HttpResponse('My name is Sergey...')
